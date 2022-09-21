8cb63b934a4b16cb212d14c31b4598869cfab733
borayilmazhan <boraylmzhn@gmail.com>
2022-09-21 18:30:10 +0300

    Initial Commit

---
+++ BootcampTaskk3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BootcampTaskk3
+//
+//  Created by Bora Yilmazhan on 21.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
